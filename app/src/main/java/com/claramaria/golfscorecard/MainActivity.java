1aa638d885be8823ab2e2516ad01c41603e28182
claaraa <clrstancu@gmail.com>
2020-04-25 17:22:15 +0300

    Implemented SharePreferences for keeping the values onPause

--- app/src/main/java/com/claramaria/golfscorecard/MainActivity.java
+++ app/src/main/java/com/claramaria/golfscorecard/MainActivity.java
@@ -10,13 +10,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
 
         int strokes = 0;
         for (int i = 0; i < mHoles.length; i++) {
+            strokes = mSharedPreferences.getInt(KEY_STROKE_COUNT + i, 0);
             mHoles[i] = new Hole("Hole" + (i + 1) + " :", strokes);
         }
 
         mListAdapter = new ListAdapter(this, mHoles);
         setListAdapter(mListAdapter);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        for(int i = 0; i < mHoles.length; i++){
+            mEditor.putInt(KEY_STROKE_COUNT + i, mHoles[i].getmStrokeCount()); //18 different keys for our sharedPreferences
+        }
+        mEditor.apply();
     }
 }

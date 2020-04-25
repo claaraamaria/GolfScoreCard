4e23c279c613715f334261ac17ee288435df748c
claaraa <clrstancu@gmail.com>
2020-04-25 16:15:48 +0300

    Initial commit

---
+++ app/src/main/java/com/claramaria/golfscorecard/MainActivity.java
@@ -0,0 +1,14 @@
+package com.claramaria.golfscorecard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

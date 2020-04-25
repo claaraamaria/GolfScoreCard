0ba8ff94833031d99ac481dc4dcf7c66d3ae569a
claaraa <clrstancu@gmail.com>
2020-04-25 17:07:01 +0300

    Created the List Adapter & Hole class

--- app/src/main/java/com/claramaria/golfscorecard/MainActivity.java
+++ app/src/main/java/com/claramaria/golfscorecard/MainActivity.java
@@ -10,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int strokes = 0;
+        for (int i = 0; i < mHoles.length; i++) {
+            mHoles[i] = new Hole("Hole" + (i + 1) + " :", strokes);
+        }
+
+        mListAdapter = new ListAdapter(this, mHoles);
+        setListAdapter(mListAdapter);
     }
 }
